32222b0464a3d509bcf2284fccef13c8260a6f5a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-12-02 23:43:40 -0200

    homepage button 01 and 02 animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
 
-    //Border animation for Nav items
+    //Border animation for nav items
     $('.navbar-nav li a').addClass('border-animation');
-})
+
+
+    // Homepage button animation
+    $('.btn-sub1').hover(
+        function() {
+            $('.fa-angle-right').css({
+                "transform" : "rotate(180deg)",
+                "margin-left" : "5px"
+            });
+        },
+
+        function() {
+            $('.fa-angle-right').css({
+                "transform" : "rotate(0deg)",
+                "margin-left" : "0px"
+            });
+        }
+    );
+
+    $('.btn-sub2').hover(
+        function() {
+            $('.fa-angle-double-right').css({
+                "transform" : "rotate(180deg)",
+                "margin-right" : "5px"
+            })
+        },
+        
+        function() {
+            $('.fa-angle-double-right').css({
+                "transform" : "rotate(0deg)",
+                "margin-right" : "0px"
+            });
+        }
+    );
+});
